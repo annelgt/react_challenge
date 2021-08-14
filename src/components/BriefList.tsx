c7cc7cfb61dd2a120ec38aa82bed5048803f5b56
Anne Bellot <bellotanne65@gmail.com>
2021-08-14 15:44:05 +0200

    Fix display conditions for blocks according to briefs list's length

--- src/components/BriefList.tsx
+++ src/components/BriefList.tsx
@@ -41,59 +41,57 @@ export class BriefList extends Component<BriefListProps, BriefListState> {
     }
 
     render() {
-        if (this.props.briefs.length === 0) {
-            return (
-                <Box mt={2} mb={2}>
-                    <Paper>
-                        <Box p={2} textAlign="center">
-                            <Typography variant="caption">Aucun article.</Typography>
-                        </Box>
-                    </Paper>
-                </Box>
-            );
-        } else {
-            return (
-                <div>
-                    <div>
-                        <TextField
-                            select
-                            disabled={this.props.loadingBriefs === true}
-                            name="productId" value={this.state.productId || ""} onChange={this.onChangeProduct}
-                            helperText="Filtrer sur le produit"
-                        >
-                            <MenuItem value="">-----------</MenuItem>
-                            {this.props.products.map((product: IProduct) => {
-                                return <MenuItem key={product.id} value={product.id}>{product.label}</MenuItem>;
-                            })}
-                        </TextField>
-                    </div>
-                    <div style={{display: this.props.loadingBriefs ? "none" : "block"}}>
-                        {this.props.briefs.map((brief: IBrief) => {
-                            return (
-                                <Box mt={2} mb={2} key={brief.id}>
-                                    <Paper>
-                                        <Box p={2}>
-                                            <Typography variant="h6">{brief.title}</Typography>
-                                            <Typography variant="subtitle1" paragraph>
-                                                {brief.comment}
-                                            </Typography>
-                                            <Typography variant="caption" display="block" gutterBottom>
-                                                {brief.product ? brief.product.label : ''}
-                                            </Typography>
-                                        </Box>
-                                    </Paper>
-                                </Box>
-                            );
+        return (
+            <div>
+                <div style={{display: this.props.briefs.length === 0 && !this.state.productId ? "none" : "block"}}>
+                    <TextField
+                        select
+                        disabled={this.props.loadingBriefs === true}
+                        name="productId" value={this.state.productId || ""} onChange={this.onChangeProduct}
+                        helperText="Filtrer sur le produit"
+                    >
+                        <MenuItem value="">-----------</MenuItem>
+                        {this.props.products.map((product: IProduct) => {
+                            return <MenuItem key={product.id} value={product.id}>{product.label}</MenuItem>;
                         })}
-                    </div>
-                    <div style={{display: this.props.loadingBriefs ? "block" : "none"}}>
-                        <Box m={2} alignItems="center" justifyContent="center" display="flex">
-                            <CircularProgress/>
-                        </Box>
-                    </div>
+                    </TextField>
                 </div>
-            );
-        }
+                <div style={{display: this.props.briefs.length === 0 ? "block" : "none"}}>
+                    <Box mt={2} mb={2}>
+                        <Paper>
+                            <Box p={2} textAlign="center">
+                                <Typography variant="caption">Aucun article.</Typography>
+                            </Box>
+                        </Paper>
+                    </Box>
+                </div>
+                <div style={{display: this.props.loadingBriefs || this.props.briefs.length === 0 ? "none" : "block"}}>
+                    {this.props.briefs.map((brief: IBrief) => {
+                        return (
+                            <Box mt={2} mb={2} key={brief.id}>
+                                <Paper>
+                                    <Box p={2}>
+                                        <Typography variant="h6">{brief.title}</Typography>
+                                        <Typography variant="subtitle1" paragraph>
+                                            {brief.comment}
+                                        </Typography>
+                                        <Typography variant="caption" display="block" gutterBottom>
+                                            {brief.product ? brief.product.label : ''}
+                                        </Typography>
+                                    </Box>
+                                </Paper>
+                            </Box>
+                        );
+                    })}
+                </div>
+                <div style={{display: this.props.loadingBriefs ? "block" : "none"}}>
+                    <Box m={2} alignItems="center" justifyContent="center" display="flex">
+                        <CircularProgress/>
+                    </Box>
+                </div>
+            </div>
+        );
+
     }
 }
 
